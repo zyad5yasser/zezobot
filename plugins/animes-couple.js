addd54f3a063aa059ec7efb85a9842009433588f
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-07 23:15:10 +0200

    Update animes-couple.js

--- plugins/animes-couple.js
+++ plugins/animes-couple.js
@@ -5,13 +5,13 @@ let handler = async (m, { conn }) => {
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \nولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \nولا تنسى متابعتي هنا \nhttps://www.instagram.com/zaidyasser44?igsh=MWkweWpwMG10bjNkeg==', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n ولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n ولا تنسى متابعتي هنا \nhttps://www.instagram.com/zaidyasser44?igsh=MWkweWpwMG10bjNkeg==', m)
 }
 handler.help = ['ppcouple','tofanime']
 handler.tags = ['anime']
-handler.command = ['ppcouple','tofanime'] 
+handler.command = ['ppcouple','tofanime','طقم-بنات'] 
 
 
 export default handler
