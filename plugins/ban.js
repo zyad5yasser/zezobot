fcd0a6b5baf7648e9b419e50cdd8ee540067bea2
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-07 22:59:43 +0200

    Update ban.js

--- plugins/ban.js
+++ plugins/ban.js
@@ -1,12 +1,12 @@
 let handler = async (m, { participants }) => {
     // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('تم حظرك من استعمال البوت يا عزيزي لانك تخالف سياسة استعماله ♥ يمكنك معرفة سياسة استخدام البوت لدى صاحبه نورالدين \ninstagram.com/noureddine_ouafy 😄!')
+    m.reply('تم حظرك من استعمال البوت يا عزيزي لانك تخالف سياسة استعماله ♥ يمكنك معرفة سياسة استخدام البوت لدى صاحبه نورالدين')
     // } else m.reply('Ada nomor host disini...')
 }
 handler.help = ['ban']
 handler.tags = ['owner']
-handler.command = /^ban$/i
+handler.command = /^ban|بان$/i
 
 handler.owner = true
 
