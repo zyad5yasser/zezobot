369c3611aabb00dc08ddcbf77d30699e8f9ad3f8
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-08 18:13:14 +0200

    Update dx.js

--- plugins/dx.js
+++ plugins/dx.js
@@ -19,7 +19,7 @@ await m.reply('وقعت مشكلة :(')
 }
 handler.help = ["dx,بوت"]
 handler.tags = ["ai"]
-handler.command = /^(بوت),(dx)$/i
+handler.command = /^(dx)$/i
 export default handler
 
 /* New Line */
