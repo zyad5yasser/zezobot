98916a5da0e9c5e9d25ea40cb462695de1d43fe0
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-11 09:14:54 +0200

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`*الموضوع:*\n${text ? `${text}\n` : ''}\n*أحد الأدمينز قام بالإشارة إليك لا أعلم ماذا يريد منك إذهب إليه في الخاص وناقش معه لماذا قام بالإشارة اليك فالبوت وصاحبه لا دخل لهم بهذا ههههه*\n\ninstagram.com/noureddine_ouafy \n\n\n` + users.map(v => '│♪ @' + v.replace(/@.+/, '')).join`\n` + '\n⛾「 *تمت الإشارة للكل* 」⛾', null, {
+    m.reply(`*الموضوع:*\n${text ? `${text}\n` : ''}\n*أحد الأدمينز قام بالإشارة إليك لا أعلم ماذا يريد منك إذهب إليه في الخاص وناقش معه لماذا قام بالإشارة اليك فالبوت وصاحبه لا دخل لهم بهذا ههههه*\n\nhttps://www.instagram.com/zaidyasser44?igsh=MWkweWpwMG10bjNkeg== \n\n\n` + users.map(v => '│♪ @' + v.replace(/@.+/, '')).join`\n` + '\n⛾「 *تمت الإشارة للكل* 」⛾', null, {
         mentions: users
     })
 }
 
-handler.help = ['tagall']
-handler.tags = ['owner']
-handler.command = ['tagall']
+handler.help = ['منشن']
+handler.tags = ['group']
+handler.command = ['منشن']
 handler.admin = true
 handler.group = true
 
