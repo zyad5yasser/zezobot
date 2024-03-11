65b65fa2d1a30445c5407be29ded22f10d063ef2
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-11 09:12:46 +0200

    Update random-C. Ronaldo.js

--- plugins/random-C. Ronaldo.js
+++ plugins/random-C. Ronaldo.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 const handler = async (m, {conn, usedPrefix, command}) => {
   const cristiano = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json`)).data;
   const ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())];
-  conn.sendFile(m.chat, ronaldo, 'error.jpg', `*دا عمك بردو بعد ميسي*`, m);
+  conn.sendFile(m.chat, ronaldo, 'error.jpg', `*سيييييي 🐦💔*`, m);
 };
 // conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['cristianoronaldo', 'cr7'];
+handler.help = ['cristianoronaldo', 'cr7','الدون'];
 handler.tags = ['internet'];
 handler.command = /^(الدون|رونالدو)$/i;
 export default handler;
