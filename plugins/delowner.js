9908088571634011cd3ad0821136d693fa5c6d3c
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-11 10:01:07 +0200

    Update delowner.js

--- plugins/delowner.js
+++ plugins/delowner.js
@@ -3,17 +3,17 @@ let handler = async (m, { conn, text }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
     if (!who) throw 'قم بالاشارة للشخص الذي ازالتت من ملكية البوت'
-    if (!global.owner.includes(who.split`@`[0])) throw 'تمت ازالته من ملكية البوت ♥!'
+    if (!global.owner.includes(who.split`@`[0])) throw '*تمت ازالته من ملكية البوت ♥!*'
     let index = global.owner.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
     global.owner.splice(index, 1)
-    const caption = `ا @${who.split`@`[0]} تم اعتباره من الان فصاعدا ليس مالكا للبوت ♥`
+    const caption = `ا @${who.split`@`[0]}*تم اعتباره من الان فصاعدا ليس مالكا للبوت ♥*`
     await conn.reply(m.chat, caption, m, {
         mentions: conn.parseMention(caption)
     });
 }
-handler.help = ['delowner']
+handler.help = ['حذف-بريم']
 handler.tags = ['owner']
-handler.command = /^delowner$/i
+handler.command = /^حذف-بريم$/i
 
 handler.owner = true
 
