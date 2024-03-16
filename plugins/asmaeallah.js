61ebe03453b5e866f5f7445699abb90f4d224f9c
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-17 00:08:05 +0200

    Update asmaeallah.js

--- plugins/asmaeallah.js
+++ plugins/asmaeallah.js
@@ -18,9 +18,9 @@ ${translation_id}
     }
     m.reply(contoh + data + anjuran)
 }
-handler.help = ['asmaeallah']
+handler.help = ['اسماء-الله']
 handler.tags = ['islam']
-handler.command = /^asmaeallah$/i
+handler.command = /^اسماء-الله$/i
 export default handler
 
 global.asmaulhusna = [
