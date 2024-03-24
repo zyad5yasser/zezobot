297d47ccc43622cf944cb081a986bdcc5c25a031
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-24 07:04:21 +0200

    Update بان شات.js

--- plugins/انطق.js
+++ plugins/انطق.js
@@ -1,45 +1,9 @@
-import gtts from 'node-gtts'
-import { readFileSync, unlinkSync } from 'fs'
-import { join } from 'path'
-
-const defaultLang = 'ar'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  let lang = args[0]
-  let text = args.slice(1).join(' ')
-  if ((args[0] || '').length !== 2) {
-    lang = defaultLang
-    text = args.join(' ')
-  }
-  if (!text && m.quoted?.text) text = m.quoted.text
-
-  let res
-  try { res = await tts(text, lang) }
-  catch (e) {
-    m.reply(e + '')
-    text = args.join(' ')
-    if (!text) throw `*هــكذا : ${usedPrefix}${command} مرحبا*`
-    res = await tts(text, defaultLang)
-  } finally {
-    if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
-  }
+let handler = async (m) => {
+global.db.data.chats[m.chat].isBanned = true
+m.reply('*[❗] تم حظر هذه الدردشة بنجاح*\n\n*—◉ سوف يتفاعل الروبوت مع أي أمر حتى تقوم بإلغاء حظر هذه الدردشة*')
 }
-handler.help = ['انطق']
-handler.tags = ['tools']
-handler.command = ['tts', 'انطق'] 
-
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^حظر|بانشات$/i
+handler.rowner = true
 export default handler
-
-function tts(text, lang = 'en-en') {
-  console.log(lang, text)
-  return new Promise((resolve, reject) => {
-    try {
-      let tts = gtts(lang)
-      let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
-      tts.save(filePath, text, () => {
-        resolve(readFileSync(filePath))
-        unlinkSync(filePath)
-      })
-    } catch (e) { reject(e) }
-  })
-}
