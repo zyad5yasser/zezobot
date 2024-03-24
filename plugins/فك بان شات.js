f7be7e3bd0872096b17b181fdeb910ba0edab3a5
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-24 23:46:42 +0200

    Update فك بان شات.js

--- plugins/فك بان شات.js
+++ plugins/فك بان شات.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
 global.db.data.chats[m.chat].isBanned = false
-m.reply('* تم إلغاء حظر هذه الدردشة بنجاح*')
+m.reply('*تم إلغاء حظر هذه الدردشة بنجاح*')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
