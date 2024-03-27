ebeaf4b887ff844cd235f1232561ed2cd1994d37
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-27 07:06:57 +0200

    Create اليكسا.js

---
+++ plugins/اليكسا.js
@@ -0,0 +1,27 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  const name = conn.getName(m.sender);
+  if (!text) {
+    throw `مرحبا *${name}*, هل تريد التحدث? Respond with *${usedPrefix + command}* (your message)\n\n📌 Example: *${usedPrefix + command}* Hi bot`;
+  }
+  
+  m.react('🗣️');
+
+  const msg = encodeURIComponent(text);
+  
+  const res = await fetch(`https://ultimetron.guruapi.tech/rekha?prompt=${msg}`);
+
+  const json = await res.json();
+  
+  
+    let reply = json.result.response;
+    m.reply(reply);
+
+};
+
+handler.help = ['اليكسا'];
+handler.tags = ['ai'];
+handler.command = ['bot', 'alexa','بوت','اليكسا'];
+
+export default handler;
