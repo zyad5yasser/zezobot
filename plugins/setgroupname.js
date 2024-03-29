2295f4ef4e538a027e00aa9dfe94cee8996858d5
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-29 03:05:28 +0200

    Update setgroupname.js

--- plugins/setgroupname.js
+++ plugins/setgroupname.js
@@ -1,12 +1,12 @@
 var handler = async (m, { conn, text, isROwner, isOwner }) => {
 
-if (!text) return conn.reply(m.chat, 'هذا الأمر خاص بتغيير إسم المجموعة تكتب هكذا \n\n*.setgoupename* BOBIZA BOT GROUPE', m,  )
+if (!text) return conn.reply(m.chat, 'هذا الأمر خاص بتغيير إسم المجموعة تكتب هكذا \n\n*.اسم_الجروب* 𝑍𝐸𝑍𝛩 𝐵𝛩𝑇 GROUPE', m,  )
 await conn.groupUpdateSubject(m.chat, text)
 conn.reply(m.chat, `🚩 ${text ? `${text} *هو الاسم الجديد لهذه المجموعة*\n` : '*لم يعطوا اسما*'}`, m,  )
 }
-handler.help = ['setgroupname']
-handler.tags = ['owner']
-handler.command = /^setgroupname$/i
+handler.help = ['اسم_الجروب']
+handler.tags = ['group']
+handler.command = /^setgroupname|اسم_الجروب$/i
 
 handler.botAdmin = true
 handler.group = true
