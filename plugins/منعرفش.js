29460d807330c832b89902ffebca0bb6a54f7e78
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-03-29 06:41:00 +0200

    Create منعرفش.js

---
+++ plugins/منعرفش.js
@@ -0,0 +1,32 @@
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/fb0407ea580df29f483a084292246df1/raw/b21cf1d3eb9cb5057ea39be715aa71af58673d27/gistfile1.txt')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+『𝑍𝐸𝑍𝛩 𝐵𝛩𝑇』
+     `.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.audio, '', caption, m, false, { mimetype: 'audio/mp4' }), // تغيير هنا
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
+}
+handler.help = ['guessflag']
+handler.tags = ['game']
+handler.command = /^اوست/i
+
+export default handler
+                                                  
