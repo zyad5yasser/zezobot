0e6b88d71c07ccbcfe88b4d997a819f91ce50936
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-05-01 00:00:57 +0300

    Update gc-setwelcome.js

--- plugins/gc-setwelcome.js
+++ plugins/gc-setwelcome.js
@@ -1,11 +1,14 @@
-const handler = async (m, {conn, text, isROwner, isOwner}) => {
+
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
-    global.db.data.chats[m.chat].sWelcome = text;
-    m.reply(tradutor.texto1);
-  } else throw `${tradutor.texto2[0]}\n*- @user (mención)*\n*- @group (nombre de grupo)*\n*- @desc (description de grupo)*`;
-};
-handler.help = ['الترحيب'];
-handler.tags = ['group'];
-handler.command = ['setwelcome','الترحيب'];
-handler.admin = true;
-export default handler;
+    global.db.data.chats[m.chat].sWelcome = text
+    m.reply('✅ The welcome message is configured')
+  } else throw `✳️ Enter the Welcome message\n\n@user (mention)\n@group (Group name)\n@desc (description of group)`
+}
+handler.help = ['الترحيب']
+handler.tags = ['group']
+handler.command = ['setwelcome','الترحيب'] 
+handler.admin = true
+handler.owner = false
+
+export default handler
