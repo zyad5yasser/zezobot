f17d10f0fcacdc7899c626e7a0efbd2a6468098a
Zyadnoh2007 <161977609+Zyadnoh2007@users.noreply.github.com>
2024-05-01 14:56:10 +0300

    Update ردصوت.js

--- plugins/ردصوت.js
+++ plugins/ردصوت.js
@@ -11,7 +11,7 @@ handler.all = async function(m, {conn}) {
   if (/^علاوي$/i.test(m.text) && !chat.isBanned) {
     if (!db.data.chats[m.chat].audios) return;
     if (!db.data.settings[this.user.jid].audios_bot && !m.isGroup) return;
-    const vn = './media/علاوي.mp3';
+    const vn = './علاوي.mp3';
     mconn.conn.sendPresenceUpdate('recording', m.chat);
     mconn.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 }
